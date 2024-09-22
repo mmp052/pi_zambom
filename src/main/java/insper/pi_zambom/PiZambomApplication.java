da1162009bb4004f75592840121ed92c70e971aa
mateus <mateusmoreirammp052@gmail.com>
2024-09-22 17:23:18 -0300

    app.propetis

--- src/main/java/insper/pi_zambom/PiZambomApplication.java
+++ src/main/java/insper/pi_zambom/PiZambomApplication.java
@@ -3,8 +3,10 @@ package insper.pi_zambom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PiZambomApplication {
 
 	public static void main(String[] args) {
